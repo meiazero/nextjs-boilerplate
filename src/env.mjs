2df2726a236aea6d408c501e0644c7f528ca0d5a
Emanuel Avila <avilaemanueel@gmail.com>
2024-10-04 18:20:59 -0300

    update envs

--- src/env.mjs
+++ src/env.mjs
@@ -2,17 +2,36 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
+  /**
+   * Specify your server-side environment variables schema here. This way you can ensure the app isn't
+   * built with invalid env vars.
+   */
   server: {
     NODE_ENV: z
       .enum(["development", "production", "test"])
       .default("development"),
   },
-  shared: {},
-  client: {
-    NEXT_PUBLIC_API_URL: z.string().default("http://localhost:3000"),
+  shared: {
+    VERCEL_ENV: z
+      .enum(["production", "preview", "development"])
+      .default("development"),
   },
+  /**
+   * Specify your client-side environment variables schema here.
+   * For them to be exposed to the client, prefix them with `NEXT_PUBLIC_`.
+   */
+  client: {
+    NEXT_PUBLIC_BASE_URL: z.string().default("http://localhost:3000"),
+    NEXT_PUBLIC_VERCEL_REVALIDATE_TIME: z.string().transform((v) => Number(v)),
+  },
+  /**
+   * Destructure all variables from `process.env` to make sure they aren't tree-shaken away.
+   */
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    VERCEL_ENV: process.env.VERCEL_ENV,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    NEXT_PUBLIC_VERCEL_REVALIDATE_TIME:
+      process.env.NEXT_PUBLIC_VERCEL_REVALIDATE_TIME,
   },
 });
