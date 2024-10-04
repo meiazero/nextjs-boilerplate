6ae5478876aceb2c623452a2a4593b045b284a7f
Emanuel Avila <avilaemanueel@gmail.com>
2024-10-04 18:18:34 -0300

    declare constants

---
+++ next.constants.mjs
@@ -0,0 +1,12 @@
+import { env } from "./src/env.mjs";
+
+/**
+ * This is used for defining a default time of when `next-data` and other dynamically generated
+ * but static-enabled pages should be regenerated.
+ *
+ * Note that this is a custom Environment Variable that can be defined by us when necessary
+ * if no value is provided, it will default to 432000 (5 days)
+ */
+export const VERCEL_REVALIDATE = Number(
+  env.NEXT_PUBLIC_VERCEL_REVALIDATE_TIME || 432000,
+);
